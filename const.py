bdff26aa3489708d38840b8d1cf8bb8c9e1d1642
Vadim Iskuchekov <vadim.deus@gmail.com>
2017-02-24 21:30:25 +1000

    Add gitignore and constants.

---
+++ const.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+CURRENCY_CODES = {
+    # http://www.cbr.ru/scripts/XML_val.asp?d=0
+    'USD': 'R01235',
+    'EUR': 'R01239',
+}
+
+CBR_API_URLS = {
+    # daily currencies
+    'daily_rus': 'http://www.cbr.ru/scripts/XML_daily.asp?',
+    'daily_eng': 'http://www.cbr.ru/scripts/XML_daily_eng.asp?'
+}
