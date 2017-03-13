bbc282b41be19f297558bc57b3dd2a7eae3b6a52
Vadim Iskuchekov <vadim.deus@gmail.com>
2017-03-13 18:04:57 +1000

    Deleted unused files.

--- cbrf/const.py
+++ cbrf/const.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
 
 CURRENCY_CODES = {
     # http://www.cbr.ru/scripts/XML_val.asp?d=0
--- cbrf/exceptions.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
-
-
-class NotImplementedException(Exception):
-    def __init__(self, *args, **kwargs):
-        super(NotImplementedException, self).__init__('Not Implemented')
